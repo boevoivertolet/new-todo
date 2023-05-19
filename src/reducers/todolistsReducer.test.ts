36fc1f1f0367c4d6b6ad9faa77a1561b5cbdb8b5
boevoivertolet <bjha78rus@yandex.ru>
2023-05-19 16:11:25 +0300

    addTodolist test

--- src/reducers/todolistsReducer.test.ts
+++ src/reducers/todolistsReducer.test.ts
@@ -1,6 +1,10 @@
 import { v1 } from 'uuid'
 import { TodolistsType } from '../App'
-import { removeTodolistAC, todolistReducer } from './todolistsReducer'
+import {
+	addTodolistAC,
+	removeTodolistAC,
+	todolistReducer
+} from './todolistsReducer'
 
 test('correct todolist should be removed', () => {
 	let todolistId1 = v1()
@@ -14,4 +18,18 @@ test('correct todolist should be removed', () => {
 
 	expect(endState.length).toBe(1)
 	expect(endState[0].id).toBe(todolistId2)
+})
+
+test('todolist should be added', () => {
+	let todolistId1 = v1()
+	let todolistId2 = v1()
+
+	const startState: Array<TodolistsType> = [
+		{ id: todolistId1, title: 'What to learn', filter: 'all' },
+		{ id: todolistId2, title: 'What to  buy', filter: 'all' }
+	]
+	const endState = todolistReducer(startState, addTodolistAC('title'))
+
+	expect(endState.length).toBe(3)
+	expect(endState[0].title).toBe('title')
 })
