d3cdd55761a93adedd25f13108675438968342b8
boevoivertolet <bjha78rus@yandex.ru>
2023-05-23 11:01:17 +0300

    correct task should be deleted from correct array

--- src/reducers/taskReducer.test.ts
+++ src/reducers/taskReducer.test.ts
@@ -1,6 +1,37 @@
-import { TasksType, TodolistsType } from '../App'
-import { tasksReducer } from './taskReducer'
+import { TasksType } from '../App'
+import { removeAC, tasksReducer } from './taskReducer'
 import { addTodolistAC, removeTodolistAC } from './todolistsReducer'
+
+test('correct task should be deleted from correct array', () => {
+	const startState: TasksType = {
+		todolistId1: [
+			{ id: '1', title: 'CSS', isDone: false },
+			{ id: '2', title: 'JS', isDone: true },
+			{ id: '3', title: 'React', isDone: false }
+		],
+		todolistId2: [
+			{ id: '1', title: 'bread', isDone: false },
+			{ id: '2', title: 'milk', isDone: true },
+			{ id: '3', title: 'tea', isDone: false }
+		]
+	}
+
+	const action = removeAC('todolistId2', '2')
+
+	const endState = tasksReducer(startState, action)
+
+	expect(endState).toEqual({
+		todolistId1: [
+			{ id: '1', title: 'CSS', isDone: false },
+			{ id: '2', title: 'JS', isDone: true },
+			{ id: '3', title: 'React', isDone: false }
+		],
+		todolistId2: [
+			{ id: '1', title: 'bread', isDone: false },
+			{ id: '3', title: 'tea', isDone: false }
+		]
+	})
+})
 
 test('new array should be added when new todolist is added', () => {
 	const startState: TasksType = {
