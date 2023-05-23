f5ab7bdd0c02df98467c1e1863e7ba359bb4abe3
boevoivertolet <bjha78rus@yandex.ru>
2023-05-23 11:04:32 +0300

    correct task should be added to correct array

--- src/reducers/taskReducer.test.ts
+++ src/reducers/taskReducer.test.ts
@@ -1,5 +1,5 @@
 import { TasksType } from '../App'
-import { removeAC, tasksReducer } from './taskReducer'
+import { addAC, removeAC, tasksReducer } from './taskReducer'
 import { addTodolistAC, removeTodolistAC } from './todolistsReducer'
 
 test('correct task should be deleted from correct array', () => {
@@ -31,6 +31,31 @@ test('correct task should be deleted from correct array', () => {
 			{ id: '3', title: 'tea', isDone: false }
 		]
 	})
+})
+
+test('correct task should be added to correct array', () => {
+	const startState: TasksType = {
+		todolistId1: [
+			{ id: '1', title: 'CSS', isDone: false },
+			{ id: '2', title: 'JS', isDone: true },
+			{ id: '3', title: 'React', isDone: false }
+		],
+		todolistId2: [
+			{ id: '1', title: 'bread', isDone: false },
+			{ id: '2', title: 'milk', isDone: true },
+			{ id: '3', title: 'tea', isDone: false }
+		]
+	}
+
+	const action = addAC('todolistId2', 'juce')
+
+	const endState = tasksReducer(startState, action)
+
+	expect(endState['todolistId1'].length).toBe(3)
+	expect(endState['todolistId2'].length).toBe(4)
+	expect(endState['todolistId2'][0].id).toBeDefined()
+	expect(endState['todolistId2'][0].title).toBe('juce')
+	expect(endState['todolistId2'][0].isDone).toBe(false)
 })
 
 test('new array should be added when new todolist is added', () => {
