2292d5e6d72ab6fe95ec46dc5bcaa3c17b1e9838
boevoivertolet <bjha78rus@yandex.ru>
2023-05-24 15:29:04 +0300

    create story for Task

---
+++ src/common/components/Task/Task.stories.tsx
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
+import { Task } from './Task'
+
+const meta: Meta<typeof Task> = {
+	title: 'TODOLISTS/Task',
+	component: Task,
+
+	tags: ['autodocs'],
+
+	args: {
+		changeTaskStatus: action('Status changed inside Task'),
+		changeTaskTitle: action('Title changed inside Task'),
+		removeTask: action('Remove Button clicked changed inside Task'),
+		title: 'JS',
+		todolistId: 'fgdosrg8rgjuh'
+	}
+}
+
+export default meta
+type Story = StoryObj<typeof Task>
+
+export const TaskIsNotDoneStory: Story = {}
+
+export const TaskIsDoneStory: Story = {
+	args: {
+		title: 'CSS'
+	}
+}
