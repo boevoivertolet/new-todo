9ab535343a4de2b4f52eaa1c700dcd63e238f01c
boevoivertolet <bjha78rus@yandex.ru>
2023-06-14 15:59:28 +0300

    create Login

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {TodolistsList} from "../features/TodolistsList";
 import s from "./App.module.scss";
 import ButtonAppBar from "../common/components/AppBar/AppBar";
+import {Login} from "../features/Login/Login";
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
 				<ButtonAppBar />
 			<div>
 				<TodolistsList/>
+
 			</div>
 		</div>
 	)
