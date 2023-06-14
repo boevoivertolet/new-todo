58918810265c2332ee5acd672c469ddf8c112bb1
boevoivertolet <bjha78rus@yandex.ru>
2023-06-14 19:19:02 +0300

    add routes

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,16 @@ import './index.scss';
 import App from './app/App';
 import {Provider} from "react-redux";
 import {store} from "./app/store";
+import {BrowserRouter} from "react-router-dom";
 
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
-    <Provider store = {store}>
-        <App />
-    </Provider>
+    <BrowserRouter>
+        <Provider store = {store}>
+            <App />
+        </Provider>
+    </BrowserRouter>
 );
