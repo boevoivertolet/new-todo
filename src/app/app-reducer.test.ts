df2ed8806174da4b58d325e5caf3bf09cc824d55
boevoivertolet <bjha78rus@yandex.ru>
2023-08-24 16:08:42 +0300

    add app reducer test / AppInitialState

---
+++ src/app/app-reducer.test.ts
@@ -0,0 +1,23 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+const slice = createSlice({
+	name: "app",
+	initialState: {
+		status: "loading" as RequestStatusType,
+		error: null as ErrorType,
+	},
+	reducers: {
+		setAppStatus: (state, action: PayloadAction<{ status: RequestStatusType }>) => {
+			state.status = action.payload.status;
+		},
+		setAppError: (state, action: PayloadAction<{ error: ErrorType }>) => {
+			state.error = action.payload.error;
+		},
+	},
+});
+export const appReducer = slice.reducer;
+export const appActions = slice.actions;
+
+//types
+export type ErrorType = string | null;
+export type RequestStatusType = "idle" | "loading" | "succeeded" | "failed";
