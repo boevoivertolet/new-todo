f9493d4e55c2ddc933d54cff533ccaa7e44a7142
boevoivertolet <bjha78rus@yandex.ru>
2023-08-24 14:07:58 +0300

    refactor auth reducer for slice

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,10 @@
 import { AnyAction, combineReducers } from "redux";
-import { tasksReducer } from "../features/taskReducer";
+import { tasksReducer } from "features/taskReducer";
 import { ThunkDispatch } from "redux-thunk";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
-import { todolistsReducer } from "../features/todolistsReducer";
+import { todolistsReducer } from "features/todolistsReducer";
 import { appReducer } from "./app-reducer";
-import { authReducer } from "../features/Login/auth-reducer";
+import { authReducer } from "features/Login/auth-reducer";
 import { configureStore } from "@reduxjs/toolkit";
 
 const rootReducer = combineReducers({
--- src/features/Login/auth-reducer.ts
+++ src/features/Login/auth-reducer.ts
@@ -1,33 +1,59 @@
 import { Dispatch } from "redux";
-import { SetAppErrorActionType, setAppStatusAC, SetAppStatusActionType } from "../../app/app-reducer";
-import { authAPI, LoginParamsType } from "../../api/auth-api";
-import { handleServerAppError, handleServerNetworkError } from "../../utils/error-utils";
+import { SetAppErrorActionType, setAppStatusAC, SetAppStatusActionType } from "app/app-reducer";
+import { authAPI, LoginParamsType } from "api/auth-api";
+import { handleServerAppError, handleServerNetworkError } from "utils/error-utils";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-const initialState = {
-    isInitialized: false,
-    isLoggedIn: false,
-    userName: "",
-};
-type InitialStateType = typeof initialState;
+// const initialState = {
+//     isInitialized: false,
+//     isLoggedIn: false,
+//     userName: "",
+// };
+// type InitialStateType = typeof initialState;
 
-export const authReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
-    switch (action.type) {
-        case "login/set_is_logged_in":
-            return { ...state, isLoggedIn: action.value };
-        case "login/initialized":
-            return { ...state, isInitialized: action.value };
-        case "login/set_user_name":
-            return { ...state, userName: action.value };
-        default:
-            return state;
-    }
-};
+const slice = createSlice({
+    name: "auth",
+    initialState: {
+        isInitialized: false,
+        isLoggedIn: false,
+        userName: "",
+    },
+    reducers: {
+        setIsLoggedIn: (state, action: PayloadAction<{ isLoggedIn: boolean }>) => {
+            // return { ...state, isLoggedIn: action.value };
+            state.isLoggedIn = action.payload.isLoggedIn;
+        },
+        setIsInitialized: (state, action: PayloadAction<{ isInitialized: boolean }>) => {
+            // return { ...state, isInitialized: action.value };
+            state.isInitialized = action.payload.isInitialized;
+        },
+        setUserName: (state, action: PayloadAction<{ userName: string }>) => {
+            // return { ...state, userName: action.value };
+            state.userName = action.payload.userName;
+        },
+    },
+});
+export const authReducer = slice.reducer;
+export const authActions = slice.actions;
+
+// export const authReducer = (state, action: ActionsType) => {
+//     switch (action.type) {
+//         case "login/set_is_logged_in":
+//             return { ...state, isLoggedIn: action.value };
+//         case "login/initialized":
+//             return { ...state, isInitialized: action.value };
+//         case "login/set_user_name":
+//             return { ...state, userName: action.value };
+//         default:
+//             return state;
+//     }
+// };
 // actions
-export const setIsLoggedInAC = (value: boolean) => ({ type: "login/set_is_logged_in", value }) as const;
+// export const setIsLoggedInAC = (value: boolean) => ({ type: "login/set_is_logged_in", value }) as const;
 
-export const setIsInitializedAC = (value: boolean) => ({ type: "login/initialized", value }) as const;
+// export const setIsInitializedAC = (value: boolean) => ({ type: "login/initialized", value }) as const;
 
-export const setUserNameAC = (value: string) => ({ type: "login/set_user_name", value }) as const;
+// export const setUserNameAC = (value: string) => ({ type: "login/set_user_name", value }) as const;
 
 // thunks
 export const logoutTC = () => (dispatch: Dispatch<ActionsType>) => {
@@ -36,8 +62,10 @@ export const logoutTC = () => (dispatch: Dispatch<ActionsType>) => {
         .logout()
         .then((res) => {
             if (res.data.resultCode === 0) {
-                dispatch(setUserNameAC(""));
-                dispatch(setIsLoggedInAC(false));
+                // dispatch(setUserNameAC(""));
+                // dispatch(setIsLoggedInAC(false));
+                dispatch(authActions.setUserName({ userName: "" }));
+                dispatch(authActions.setIsLoggedIn({ isLoggedIn: false }));
                 dispatch(setAppStatusAC("succeeded"));
             } else {
                 handleServerAppError(res.data, dispatch);
@@ -54,7 +82,8 @@ export const loginTC = (data: LoginParamsType) => (dispatch: Dispatch<ActionsTyp
         .login(data)
         .then((res) => {
             if (res.data.resultCode === 0) {
-                dispatch(setIsLoggedInAC(true));
+                // dispatch(setIsLoggedInAC(true));
+                dispatch(authActions.setIsLoggedIn({ isLoggedIn: true }));
                 dispatch(setAppStatusAC("succeeded"));
             } else {
                 handleServerAppError(res.data, dispatch);
@@ -70,12 +99,16 @@ export const meTC = () => (dispatch: Dispatch<ActionsType>) => {
         .me()
         .then((res) => {
             if (res.resultCode === 0) {
-                dispatch(setUserNameAC(res.data.login));
-                dispatch(setIsLoggedInAC(true));
-                dispatch(setIsInitializedAC(true));
+                // dispatch(setUserNameAC(res.data.login));
+                // dispatch(setIsLoggedInAC(true));
+                // dispatch(setIsInitializedAC(true));
+                dispatch(authActions.setUserName({ userName: res.data.login }));
+                dispatch(authActions.setIsLoggedIn({ isLoggedIn: true }));
+                dispatch(authActions.setIsInitialized({ isInitialized: true }));
                 dispatch(setAppStatusAC("succeeded"));
             } else {
-                dispatch(setIsInitializedAC(true));
+                // dispatch(setIsInitializedAC(true));
+                dispatch(authActions.setIsInitialized({ isInitialized: true }));
                 handleServerAppError(res, dispatch);
             }
         })
